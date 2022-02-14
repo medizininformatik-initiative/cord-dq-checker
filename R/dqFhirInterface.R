d0e1acb92a0e9dd99817eaf69bbc4e2391b41fe6
kais.tahar@med.uni-goettingen.de <kais.tahar@med.uni-goettingen.de>
2022-02-14 15:28:39 +0100

    add outlier indicator

--- R/dqFhirInterface.R
+++ R/dqFhirInterface.R
@@ -1,56 +1,89 @@
 library(fhircrackr)
+library(lubridate)
 
-# fhir search request
+#define fhir search request
 searchRequest <- paste0(
   path,
-  'Condition?'
+  'Condition?',
+  '&_include=Condition:subject:Patient',
+  "&_include=Condition:encounter"
 )
 
 # get fhir bundles
-bundles <- fhir_search(searchRequest, max_bundles =50)
+bundles <- fhir_search(searchRequest, max_bundles =50) # Inf
 # define design parameters of condition resource
 design <- list(
   Conditions = list(
     resource = "//Condition",
     cols = list(
-      patient_id = "subject/reference",
-      encounter_id = "encounter/reference",
+      # condition_id = "id",
+      patId = "subject/reference",
+      encId= "encounter/reference",
       text= "code/text",
       display = "code/coding/display",
       code = "code/coding/code",
       system = "code/coding/system"
-
+      
     ),
     style = list(
       sep = " / ",
       brackets = c("[", "]")
     )
+  ),
+  
+  #Patient resources
+  Patients = list(
+    resource = "//Patient",
+    cols = list(
+      instId="meta/source",
+      patId= "id",
+      birthdate = "birthDate",
+      gender = "gender"
+    ),
+    style = list(
+      sep = " % ",
+      brackets = c("[", "]")
+    )
+  ),
+  
+  #Encounter resources
+  Encounters = list(
+    resource = "//Encounter",
+    cols = list(
+      patId= "subject/reference",
+      enId = "identifier/value",
+      start = "period/start",
+      end = "period/end"
+      
+    )
   )
 )
 
-# convert and save fhir bundles to df condRaw
+# convert and save fhir bundles to a data frame condRaw
 condRaw <- fhir_crack(bundles, design)$Conditions
 
 #sort out codes
 condTmp1 <- fhir_melt(condRaw,
-                                columns = c("code", "system", "display"),
-                                brackets = c("[","]"),
-                                sep = " / ",
-                                all_columns = T)
+                      # columns = c("code", "system", "display", "text"),
+                      columns = c("code", "system", "display"),
+                      brackets = c("[","]"),
+                      sep = " / ",
+                      all_columns = T)
 
 
 condTmp2 <- fhir_melt(condTmp1,
-                                columns = c("code", "system", "display"),
-                                brackets = c("[","]"),
-                                sep = " / ",
-                                all_columns = T)
+                      #columns = c("code", "system", "display", "text"),
+                      columns = c("code", "system", "display"),
+                      brackets = c("[","]"),
+                      sep = " / ",
+                      all_columns = T)
 
 # clean up conditions
 condTmp3 <- fhir_rm_indices(condTmp2, brackets = c("[", "]"))
 
-# remove "Patient/" and "Encounter/"
-condTmp3$patient_id <- sub("Patient/", "", condTmp3$patient_id)
-condTmp3$encounter_id <- sub("Encounter/", "", condTmp3$encounter_id)
+# remove "Patient/" and Encounter
+condTmp3$patId<- sub("Patient/", "", condTmp3$patId)
+condTmp3$encId <- sub("Encounter/", "", condTmp3$encId)
 condTmp3$resource_identifier <- NULL
 
 # remove duplicate patients
@@ -61,28 +94,38 @@ condIcd <- condTmp3[condTmp3$system=="http://fhir.de/CodeSystem/dimdi/icd-10-gm"
 condOrpha <- condTmp3[condTmp3$system=="http://www.orpha.net",]
 condAlphaID <- condTmp3[condTmp3$system=="http://fhir.de/CodeSystem/dimdi/alpha-id",]
 
-# split icd code in pri and sec codes
+# split icd code in pri and sec code
 condIcd$pri_code <- ifelse(nchar(condIcd$code)>6,sapply(strsplit(condIcd$code,' '), function(x) x[1]),condIcd$code)
 condIcd$sec_code <- ifelse(nchar(condIcd$code)>6,sapply(strsplit(condIcd$code,' '), function(x) x[2]),'-')
-
 #clean up
 condIcd$system <- NULL
 condIcd$code <- NULL
-names(condIcd) <- c("PatId", "EncId", "Diagnosetext", "ICD_Text","ICD_Primaerkode", "ICD_Manifestation")
+names(condIcd) <- c("PatientIdentifikator","Aufnahmenummer", "Diagnosetext", "ICD_Text","ICD_Primaerkode", "ICD_Manifestation")
 
-# Orpha and AlphaID data
+# OrphaI and AlphaID data
 condOrpha$system <- NULL
 condOrpha$display <- NULL
-names(condOrpha) <- c("PatId", "EncId", "Diagnosetext", "Orpha_Kode")
+names(condOrpha) <- c("PatientIdentifikator","Aufnahmenummer", "Diagnosetext", "Orpha_Kode")
 condAlphaID$system <- NULL
 condAlphaID$display <- NULL
-names(condAlphaID) <- c("PatId", "EncId", "Diagnosetext", "AlphaID_Kode")
+names(condAlphaID) <- c("PatientIdentifikator","Aufnahmenummer", "Diagnosetext", "AlphaID_Kode")
 
 # join condition data
 conditions <-Reduce(function(x, y) merge(x, y, all=T), list(condIcd,condOrpha, condAlphaID))
-institution_id <- unlist(strsplit(conditions$PatId,'-P-'))[ c(TRUE,FALSE) ]
-instData <- cbind( institution_id, conditions)
-headers <- c ("Institut_ID","PatientIdentifikator","Aufnahmenummer","DiagnoseText","ICD_Text","ICD_Primaerkode","ICD_Manifestation","Orpha_Kode","AlphaID_Kode")
-names(instData)<-headers
+
+# convert and save fhir bundles to a data frame condRaw
+patRaw <- fhir_crack(bundles, design)$Patients
+patients <- fhir_rm_indices(patRaw, brackets = c("[", "]"))
+patients$instId<- gsub("#.*","\\1",patients$instId)
+patients$birthdate <- year(as.Date(patients$birthdate))
+names(patients) <- c("Institut_ID","PatientIdentifikator", "Geburtsdatum", "Geschlecht")
+entRaw <- fhir_crack(bundles, design)$Encounters
+encounters <- entRaw
+encounters$patId <- sub("Patient/", "", entRaw$patId)
+encounters$start <- as.Date(encounters$start)
+encounters$end <- as.Date(encounters$end)
+names(encounters) <- c("PatientIdentifikator","Aufnahmenummer","Aufnahmedatum", "Entlassungsdatum")
+instData<-Reduce(function(x, y) merge(x, y, all=T), list(patients,encounters,conditions))
+headers <- c ("PatientIdentifikator","Aufnahmenummer", "Institut_ID", "Geburtsdatum", "Geschlecht", "Aufnahmedatum", "Entlassungsdatum" , "DiagnoseText","ICD_Text","ICD_Primaerkode","ICD_Manifestation","Orpha_Kode","AlphaID_Kode")
 
 
--- cordDqChecker.R
+++ cordDqChecker.R
@@ -1,5 +1,3 @@
-
-# Last Change at 28.01.2022
 # Kais Tahar
 # Data quality analysis for CORD
 
@@ -11,7 +9,8 @@ setwd("./")
 rm(list = ls())
 library(devtools)
 install_github("https://github.com/KaisTahar/dqLib")
-
+library(stringi)
+library(writexl)
 
 ########## data import #############
 # import CORD med data
@@ -23,23 +22,24 @@ path="http://141.5.101.1:8080/fhir/"
 #path="./Data/medData/dqTestData_KT.csv"
 #path="./Data/medData/dqTestData_KT.xlsx"
 
-
 medData <- NULL
 if (grepl("fhir", path))
 {
-  source("./R/dqFhirInterface.R")
-  medData <-instData
+  #source("./R/dqFhirInterface.R")
+  source("./R/dqFhirInterface_v2.R")
+  year <-2020
+  medData<- instData[ format(as.Date(instData$Entlassungsdatum, format="%Y-%m-%d"),"%Y")==year, ]
 }else{ ext <-getFileExtension (path)
       if (ext=="csv") medData <- read.table(path, sep=";", dec=",",  header=T, na.strings=c("","NA"), encoding = "latin1")
       if (ext=="xlsx") medData <- read.xlsx(path, sheet=1,skipEmptyRows = TRUE)
 }
 if (is.null (medData)) stop("Keine Daten vorhanden")
-
+inpatientCases <- 202
 # import CORD Ref. Data
 refData1 <- read.table("./Data/refData/cordDqList.csv", sep=",",  dec=",", na.strings=c("","NA"), encoding = "UTF-8")
 refData2 <- read.table("./Data/refData/icd10gm2020_alphaid_se_muster_edvtxt_20191004.txt", sep="|",  dec=",", na.strings=c("","NA"), encoding = "UTF-8")
 names(medData)
-mdHeader <- c ("Institut_ID","PatientIdentifikator","Aufnahmenummer","DiagnoseText","ICD_Text","ICD_Primaerkode","ICD_Manifestation","Orpha_Kode","AlphaID_Kode")
+mdHeader <- c ("Institut_ID","PatientIdentifikator","Aufnahmenummer","Diagnosetext","ICD_Text","ICD_Primaerkode","ICD_Manifestation","Orpha_Kode","AlphaID_Kode")
 headerRef1<- c ("IcdCode", "OrphaCode", "Type")
 headerRef2<- c ("Gueltigkeit", "Alpha_ID", "ICD_Primaerkode1", "ICD_Manifestation", "ICD_Zusatz","ICD_Primaerkode2", "Orpha_Kode", "Label")
 diff <- setdiff (mdHeader, names (medData))
@@ -55,17 +55,20 @@ dim (medData)
 ########## DQ Analysis #############
 cdata <- data.frame(
   basicItem=
-    c ("PatientIdentifikator","Aufnahmenummer","DiagnoseText","ICD_Text","ICD_Primaerkode","Orpha_Kode", "Total")
+    c ("PatientIdentifikator","Aufnahmenummer", "Institut_ID",  "Geschlecht", "Diagnosetext","ICD_Text","ICD_Primaerkode","Orpha_Kode","AlphaID_Kode", "Total")
+)
+ddata <- data.frame(
+  basicItem=
+    c ( "Geburtsdatum",  "Aufnahmedatum", "Entlassungsdatum", "Total")
 )
 tdata <- data.frame(
   pt_no =NA, case_no =NA
 )
 repCol=c( "PatientIdentifikator", "Aufnahmenummer", "ICD_Primaerkode","Orpha_Kode")
-setGlobals(medData, repCol, cdata, tdata)
-env$dq$missing_item<-""
-env$dq$missing_value<-""
+setGlobals(medData, repCol, cdata, ddata, tdata)
 items <- setdiff (cdata$basicItem ,c ("ICD_Primaerkode","Orpha_Kode", "Total"))
-cdata <- getMissing(items, "missing_value", "missing_item")$cdata
+cdata <- getMissing(cdata,  cdata$basicItem, "missing_value", "missing_item")
+ddata <- getMissing(ddata,  ddata$basicItem,"missing_value", "missing_item")
 env$dq$dq_msg<-""
 td <- NULL
 l  <- NULL
@@ -73,12 +76,11 @@ if (!is.empty(env$medData$Institut_ID)){
   inst <- levels(as.factor(medData$Institut_ID))
   for (i in 1:length (inst)) {
     instID <- as.character (inst[i])
-    out <-checkCordDQ(instID, refData1,refData2, "dq_msg")
-    cdata <-out$cdata
-    # statistic 
-    tdata<-cbind (getDQStatis(cdata, "basicItem", "Total"),  out$tdata)
+    out <-checkCordDQ(instID, inpatientCases, cdata, ddata, refData1, refData2, "dq_msg")
+    test<-getTotalStatistic(out$cdata, out$ddata, "basicItem", "Total")
+    tdata<-cbind (getTotalStatistic(out$cdata, out$ddata, "basicItem", "Total"),  out$tdata)
     l <- rbind (l, tdata)
-    td<-rbind(td,subset(tdata, select= c( inst_id, missing_value_rate, orphaCoding_completeness, uniqueness_rate, orphaCoding_plausibility_rate, orphaCoding_relativeFrequency, orphaCoding_absoluteFrequency,  uniqueRd_no, pt_no, case_no)))
+    td<-rbind(td,subset(tdata, select= c( inst_id, missing_value_rate, outlier_rate, orphaCoding_completeness, uniqueness_rate, orphaCoding_plausibility_rate, orphaCoding_relativeFrequency, orphaCoding_absoluteFrequency,  uniqueRd_no, pt_no, case_no)))
     }
   
 ########## DQ-Report ###################
