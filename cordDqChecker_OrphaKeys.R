a91474e925b9dfd9d3ac49b28261423c1734654f
kais.tahar@med.uni-goettingen.de <kais.tahar@med.uni-goettingen.de>
2022-03-16 15:44:19 +0100

    remove dq indicators

--- cordDqChecker_OrphaKeys.R
+++ cordDqChecker_OrphaKeys.R
@@ -1,6 +1,6 @@
 #######################################################################################################
 # Kais Tahar
-# Data quality analysis and reporting for CORD
+# Data quality key numbers for CORD-MI
 #######################################################################################################
 rm(list = ls())
 setwd("./")
@@ -27,7 +27,7 @@ cat ("\n ####################################### Data Import ###################
 # Setting path and variables
 #------------------------------------------------------------------------------------------------------
 # Export file name
-exportFile = "DQ-Report_fhirTestData"
+exportFile = "KeyNum-Report_fhirTestData"
 # report year
 reportYear <-2020
 # inpatient case number
@@ -46,11 +46,9 @@ max_FHIRbundles <- 50 # Inf
 bItemCl <-"basicItem"
 totalRow <-"Total"
 cdata <- data.frame(
-  basicItem= c("PatientIdentifikator","Aufnahmenummer", "Institut_ID",  "Geschlecht","ICD_Primaerkode","Orpha_Kode", "Total")
+  basicItem= c("PatientIdentifikator","Aufnahmenummer","ICD_Primaerkode","Orpha_Kode", "Total")
 )
-ddata <- data.frame(
-  basicItem= c ( "Geburtsdatum",  "Aufnahmedatum", "Entlassungsdatum", "Diagnosedatum", "Total")
-)
+ddata <-NULL
 oItem = c("Orpha_Kode")
 tdata <- data.frame(
   pt_no =NA, case_no =NA
@@ -75,8 +73,7 @@ if (is.null(path) | path=="")  stop("No path to data") else {
   if (grepl("fhir", path))
   {
     source("./R/dqFhirInterface.R")
-    #medData<- instData[ format(as.Date(instData$Entlassungsdatum, format="%Y-%m-%d"),"%Y")==reportYear, ]
-    medData <- instData
+    medData<- instData[ format(as.Date(instData$Entlassungsdatum, format="%Y-%m-%d"),"%Y")==reportYear, ]
   }else{ ext <-getFileExtension (path)
   if (ext=="csv") medData <- read.table(path, sep=";", dec=",",  header=T, na.strings=c("","NA"), encoding = "latin1")
   if (ext=="xlsx") medData <- read.xlsx(path, sheet=1,skipEmptyRows = TRUE)
@@ -103,49 +100,28 @@ if (!is.empty(medData$Institut_ID)){
     repMeta= c("inst_id", "report_year")
     
     #------------------------------------------------------------------------------------------------------
-    # Setting DQ dimensions , indicators and key numbers
-    #------------------------------------------------------------------------------------------------------
-    ############## Selection of DQ dimensions and indicators #########
-    # select DQ indicators for completeness dimension
-    compInd= c(
-               "missing_item_rate", 
-               "missing_value_rate", 
-               "orphaCoding_completeness_rate"
-               )
-    # select DQ indicators for plausibility dimension
-    plausInd= c( 
-                "outlier_rate", 
-                 "orphaCoding_plausibility_rate"
-               )
-    # select DQ indicators for uniqueness dimension
-    uniqInd= c(
-               "rdCase_uniqueness_rate"
-              )
-    # select DQ indicators for concordance
-    concInd= c(
-                "unique_rdCase_relativeFrequency", 
-               "orphaCoding_relativeFrequency"
-               )
-    
+    # Setting DQ key numbers
+
     ############ Selection of DQ key numbers ########################
     # select  key numbers for DQ report
     dqKeyNo= c(
-               "orphaCoding_no",  
-               "unique_rdCase_no", 
-               "rdCase_no",
-               "case_no", 
-               "patient_no", 
-               "inpatientCases_no"
-               )
-    dqRepCol <- c(repMeta, compInd, plausInd, uniqInd, concInd, dqKeyNo)
+      "orphaCoding_no",  
+      "unique_rdCase_no", 
+      "rdCase_no",
+      "case_no", 
+      "patient_no", 
+      "inpatientCases_no"
+    )
+    dqRepCol <- c(repMeta, dqKeyNo)
     # DQ report
     dqRep <-checkCordDQ(instID, reportYear, inpatientCases, refData1, refData2, dqRepCol, "dq_msg", "basicItem", "Total", oItem)
   }
   
   ################################################### DQ Reports ########################################################
   path<- paste ("./Data/Export/", exportFile, "_", dqRep$report_year,  sep = "")
-  getReport( repCol, "dq_msg", dqRep, path)
-  path <- paste(path,".xlsx",sep = "")
+  #getReport( dqRep, path)
+  #path <- paste(path,".xlsx",sep = "")
+  write.xlsx(dqRep, sheetName ="DQ Key Number", paste (path,".xlsx"))
   mItem <- setdiff  (union(env$cdata[, bItemCl], env$ddata[, bItemCl]),union (totalRow ,dItem))
   top <- paste ("\n \n ####################################***CordDqChecker***###########################################")
   msg <- paste ("\n Data quality analysis for location:", dqRep$inst_id,
@@ -154,13 +130,10 @@ if (!is.empty(medData$Institut_ID)){
                 "\n Analyzed cases:", dqRep$case_no,
                 "\n Analyzed patients:", dqRep$patient_no,
                 "\n Coded rdCases:", dqRep$rdCase_no,
-                "\n Unique rdCases:", dqRep$unique_rdCase_no,
-                "\n Missing item rate:", dqRep$missing_item_rate,
-                "\n Missing value rate:", dqRep$missing_value_rate,
-                "\n OrphaCoding completeness rate:", dqRep$orphaCoding_completeness_rate,
-                "\n OrphaCoding plausibility rate:", dqRep$orphaCoding_plausibility_rate,
-                "\n RdCase uniqueness rate:", dqRep$rdCase_uniqueness_rate)
-  if (dqRep$missing_item_rate >0)   msg <- paste (msg, "\n Following items are missing:", toString(mItem))
+                "\n Unique rdCases:", dqRep$unique_rdCase_no
+  )
+  
+  if (!is.empty(mItem))   msg <- paste (msg, "\n Following items are missing:", toString(mItem))
   msg <- paste(msg, 
                "\n \n ########################################## Export ################################################")
   msg <- paste (msg, "\n \n For more infos about data quality indicators see the generated report \n >>> in the file path:", path)
